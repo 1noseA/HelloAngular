74ccf9a3daad9a8814754399bb30c44b90a5a44a
1noseA <1noseA2L@gmail.com>
2021-11-13 23:36:26 +0900

    ルーティング

--- WebContent/js/indexController.js
+++ WebContent/js/indexController.js
@@ -1,8 +1,28 @@
-var helloApp = angular.module("helloSample", ['ngResource']);
-helloApp.controller("helloApp", function($scope, $resource){
+var helloApp = angular.module("helloSample", ['ngResource', 'ngRoute']);
+
+helloApp.config(function($routeProvider, $locationProvider) {
+	$locationProvider.hashPrefix('');
+	$routeProvider
+		.when('/edit/:empno', {
+			templateUrl : 'edit.html',
+			controller : 'editController'
+		})
+		.when('/', {
+			templateUrl : 'list.html',
+			controller : 'listController'
+		})
+		.otherwise({ redirectTo : '/' });
+});
+
+helloApp
+	.controller("listController", function($scope, $resource){
 
 	var employee = $resource("rest/employee/all");
 	/* 複数 */
 	$scope.list = employee.query();
 
-});
+	})
+	.controller('editController', function($scope, $resource, $routeParams) {
+		var employee = $resource("rest/employee/emp/" + $routeParams.empno);
+		$scope.target = employee.get();
+	});
